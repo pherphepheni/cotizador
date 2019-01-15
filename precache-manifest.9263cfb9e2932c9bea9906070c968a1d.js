8471d0c851d9aec7d16e5da95dde877013ee6222
pherphepheni <46635978+pherphepheni@users.noreply.github.com>
2019-01-15 01:50:46 -0300

    Updates

---
+++ precache-manifest.9263cfb9e2932c9bea9906070c968a1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f96ab5e68aead5e091",
+    "url": "/cotizador/static/js/runtime~main.e7f96ab5.js"
+  },
+  {
+    "revision": "ac51cdb1986f8b964660",
+    "url": "/cotizador/static/js/main.ac51cdb1.chunk.js"
+  },
+  {
+    "revision": "264a1d76d9ea5a07b5c4",
+    "url": "/cotizador/static/js/1.264a1d76.chunk.js"
+  },
+  {
+    "revision": "ac51cdb1986f8b964660",
+    "url": "/cotizador/static/css/main.d97e047c.chunk.css"
+  },
+  {
+    "revision": "6b91747add9b7fadc8432486b6adc65c",
+    "url": "/cotizador/index.html"
+  }
+];
